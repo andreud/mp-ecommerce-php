9def016b3c7857e0b5fb9720cf3d593d1ee55f15
andres <andresxv@gmail.com>
2019-09-22 17:21:29 -0300

    Se integra .env para extraer credenciales

--- process-payment.php
+++ process-payment.php
@@ -1,18 +1,22 @@
 <?php 
-
 require __DIR__  . '/vendor/autoload.php';
 //use \MercadoPago;
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+$MP_ClientId = getenv('MERCADOPAGO_CLIENT_ID');
+$MP_ClientSecret = getenv('MERCADOPAGO_CLIENT_SECRET');
 
 # For API or custom checkout:
 //\MercadoPago\SDK::setAccessToken("YOUR_ACCESS_TOKEN");      // On Production
 //\MercadoPago\SDK::setAccessToken("YOUR_TEST_ACCESS_TOKEN"); // On Sandbox
 
 # For Web-checkout:
-\MercadoPago\SDK::setClientId("1639199754827910");//ENV_CLIENT_ID
-//\MercadoPago\SDK::setClientSecret("");
+\MercadoPago\SDK::setClientId($MP_ClientId);
+\MercadoPago\SDK::setClientSecret($MP_ClientSecret);
 
+# Generar preferencia de pago a partir del form recibido
 $preferece = new \MercadoPago\Preference();
-
 $preferece->items = [
 	[
 		"title" => $_POST['title'],
@@ -22,21 +26,23 @@ $preferece->items = [
         "unit_price" => (float)$_POST['price'] 
     ]
 ];
-
 $preferece->save();
 
 if($preferece->sandbox_init_point){
 	echo $preferece->sandbox_init_point;
+	//header("Location: ".$preferece->sandbox_init_point);
+	//die();
 } else {
 	echo '<p>No se pudo generar la url para pagar</p>';
 }
 
-
-
 ?>
 
-<pre> <?php var_dump($_POST);  ?></pre>
-<pre> <?php var_dump($preferece); ?></pre>
-<pre> <?php var_dump($preferece->sandbox_init_point); ?></pre>
-
- 
+<?
+/*
+<pre><?php //var_dump($MP_ClientSecret); ?></pre>
+<pre> <?php //var_dump($_POST);  ?></pre>
+<pre> <?php //var_dump($preferece); ?></pre>
+<pre> <?php //var_dump($preferece->sandbox_init_point); ?></pre>
+*/
+?>
