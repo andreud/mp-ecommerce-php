536c308e787c63c428ea48f2204360c784b4f607
andres <andresxv@gmail.com>
2019-09-23 09:45:18 -0300

    s esuman credenciales tipo API y mas datos a la preferencia

--- process-payment.php
+++ process-payment.php
@@ -4,20 +4,24 @@ require __DIR__  . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
+$BASE_URL = getenv('BASE_URL');
 $MP_ClientId = getenv('MERCADOPAGO_CLIENT_ID');
 $MP_ClientSecret = getenv('MERCADOPAGO_CLIENT_SECRET');
 
+$MP_PublicKey = getenv('MERCADOPAGO_PUBLIC_KEY');
+$MP_AccessToken = getenv('MERCADOPAGO_ACCESS_TOKEN');
+
 # For API or custom checkout:
-//\MercadoPago\SDK::setAccessToken("YOUR_ACCESS_TOKEN");      // On Production
-//\MercadoPago\SDK::setAccessToken("YOUR_TEST_ACCESS_TOKEN"); // On Sandbox
+//\MercadoPago\SDK::setAccessToken($MP_AccessToken);      // On Production
+\MercadoPago\SDK::setAccessToken($MP_AccessToken); // On Sandbox
 
 # For Web-checkout:
 \MercadoPago\SDK::setClientId($MP_ClientId);
 \MercadoPago\SDK::setClientSecret($MP_ClientSecret);
 
 # Generar preferencia de pago a partir del form recibido
-$preferece = new \MercadoPago\Preference();
-$preferece->items = [
+$preference = new \MercadoPago\Preference();
+$preference->items = [
 	[
 		"title" => $_POST['title'],
         "description" => $_POST['title'],
@@ -26,11 +30,24 @@ $preferece->items = [
         "unit_price" => (float)$_POST['price'] 
     ]
 ];
-$preferece->save();
+// Payer:
+$payer = new stdClass();
+$payer->email = 'andresxv@gmail.com';
+$preference->payer = $payer;
+//$preference->payer = ['email' => 'andresxv@gmail.com']; // asi esta en el ejemplo de github
+//$preference->payer->email = 'andresxv@gmail.com';
 
-if($preferece->sandbox_init_point){
-	echo $preferece->sandbox_init_point;
-	//header("Location: ".$preferece->sandbox_init_point);
+$preference->back_urls = [
+	'success' => $BASE_URL . 'back_urls/success.php',
+	'pending' => $BASE_URL . 'back_urls/pending.php',
+	'failure' => $BASE_URL . 'back_urls/failure.php',
+];
+$preference->notification_url = $BASE_URL . 'notification_url.php';
+$preference->save();
+
+if($preference->sandbox_init_point){
+	echo $preference->sandbox_init_point;
+	//header("Location: ".$preference->sandbox_init_point);
 	//die();
 } else {
 	echo '<p>No se pudo generar la url para pagar</p>';
@@ -42,7 +59,7 @@ if($preferece->sandbox_init_point){
 /*
 <pre><?php //var_dump($MP_ClientSecret); ?></pre>
 <pre> <?php //var_dump($_POST);  ?></pre>
-<pre> <?php //var_dump($preferece); ?></pre>
-<pre> <?php //var_dump($preferece->sandbox_init_point); ?></pre>
+<pre> <?php //var_dump($preference); ?></pre>
+<pre> <?php //var_dump($preference->sandbox_init_point); ?></pre>
 */
 ?>
