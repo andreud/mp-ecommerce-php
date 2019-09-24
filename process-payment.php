57f526ef74bcac61a844560ef5adee730bb42551
andres <andresxv@gmail.com>
2019-09-23 22:51:39 -0300

    se vuelve a getenv y se comenta la libreria dotenv

--- process-payment.php
+++ process-payment.php
@@ -2,14 +2,14 @@
 require __DIR__  . '/vendor/autoload.php';
 //use \MercadoPago;
 
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-$dotenv->load();
-$BASE_URL = $_ENV['BASE_URL'];
-$MP_ClientId = $_ENV['MERCADOPAGO_CLIENT_ID'];
-$MP_ClientSecret = $_ENV['MERCADOPAGO_CLIENT_SECRET'];
+//$dotenv = Dotenv\Dotenv::create(__DIR__);
+//$dotenv->load();
+$BASE_URL = getenv('BASE_URL');
+$MP_ClientId = getenv('MERCADOPAGO_CLIENT_ID');
+$MP_ClientSecret = getenv('MERCADOPAGO_CLIENT_SECRET');
 
-$MP_PublicKey = $_ENV['MERCADOPAGO_PUBLIC_KEY'];
-$MP_AccessToken = $_ENV['MERCADOPAGO_ACCESS_TOKEN'];
+$MP_PublicKey = getenv('MERCADOPAGO_PUBLIC_KEY');
+$MP_AccessToken = getenv('MERCADOPAGO_ACCESS_TOKEN');
 
 # For API or custom checkout:
 //\MercadoPago\SDK::setAccessToken($MP_AccessToken);      // On Production
